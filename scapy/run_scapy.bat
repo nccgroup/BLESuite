c61773685db8a752f88b0e2e9cfb5ec72c11931e
Jean-Michel Picod <jmichel@google.com>
2019-04-02 15:04:38 +0200

    Move scapy as a requirement and use upstream repo.

--- scapy/run_scapy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set PYTHONPATH=%~dp0
-IF "%PYTHON%" == "" set PYTHON=python3
-WHERE %PYTHON% >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 set PYTHON=python
-%PYTHON% -m scapy %*
-title Scapy - dead
-PAUSE
